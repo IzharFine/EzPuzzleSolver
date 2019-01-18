362bb317112c2de585a2f5559404482f7f659f36
IzharFine <izharf@gmail.com>
2019-01-18 14:17:26 +0200

    Update board.js

--- Scripts/board.js
+++ Scripts/board.js
@@ -38,7 +38,7 @@ class Board {
                 break;
             }
         }
-        this.size=Math.sqrt(cardsArr.length+1);
+        this.size=Math.sqrt(cardsArr.length);
         this.length=cardsArr.length;
     }
 
